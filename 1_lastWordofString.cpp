c4c71d8da64d22c63a407fe66289d6316ee73382
chenyzcs <chenyzcs@163.com>
2017-08-07 16:33:52 +0800

    figure out last word of string

--- 1_lastWordofString.cpp
+++ 1_lastWordofString.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-
+    string str;
+    while (getline(cin, str))
+    {
+        size_t sz = str.size();
+        int i = sz - 1;
+        size_t cnt = 0;
+        while (i >= 0 && str[i] != ' ')
+        {
+            i--;
+            cnt++;
+        }
+        cout << cnt << endl;
+    }
     return 0;
 }
